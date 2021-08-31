2473bd9e65a1c560c0b00d3326de1ec463f99d78
JuCo168 <jco@ucsd.edu>
2021-08-30 20:32:42 -0700

    inital commit

---
+++ src/main/java/com/apartmentchecklist/apartmentchecklist/ApartmentchecklistApplication.java
@@ -0,0 +1,13 @@
+package com.apartmentchecklist.apartmentchecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentchecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentchecklistApplication.class, args);
+	}
+
+}
