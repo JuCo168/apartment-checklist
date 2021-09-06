78d64299dba386a5f05d0b4f610a847c94475455
JuCo168 <jco@ucsd.edu>
2021-09-05 23:12:47 -0700

    Created tenants Java class

---
+++ src/main/java/com/apartmentchecklist/apartmentchecklist/model/Tenants.java
@@ -0,0 +1,10 @@
+package com.apartmentchecklist.apartmentchecklist.model;
+
+import java.io.Serializable;
+
+public class Tenants implements Serializable{
+    private String first;
+    private String last;
+    private String email;
+    private String phone;
+}
